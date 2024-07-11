a10e503dcae2d00e9e9cc2581cb7f74532e3b4ae
sindooja044 <165017108+sindooja044@users.noreply.github.com>
2024-07-11 17:32:26 +0530

    Update settings.py

--- flight_search/settings.py
+++ flight_search/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&07l=wpx93ptsa#v5b48&8wv$3(_d7$qlgp9c%vpw*p%!-(yff
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
