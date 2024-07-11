d8502152fdca1bcc50e532691c564f386cb8958d
sindooja044 <165017108+sindooja044@users.noreply.github.com>
2024-07-11 18:02:00 +0530

    Update settings.py

--- flight_search/settings.py
+++ flight_search/settings.py
@@ -116,10 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'search/static'),
+    os.path.join(BASE_DIR, 'search/static/search/logo.png'),
 ]
 
 # Default primary key field type
